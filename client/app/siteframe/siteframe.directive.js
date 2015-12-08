d84defb78df07c5229a6b9dd92a65a2979a2e3c7
Stanislav Palatnik <yankees27an@gmail.com>
2015-12-07 23:25:02 -0500

    Directive logic to open new window and coordinate session

--- client/app/siteframe/siteframe.directive.js
+++ client/app/siteframe/siteframe.directive.js
@@ -5,34 +5,32 @@ angular.module('weblistSavenub')
     return {
       templateUrl: 'app/siteframe/siteframe.html',
       restrict: 'E',
-      controller: function(scope) {
+      controller: function($scope) {
         this.openSite = function(site) {
           var sessionTab = window.open("http://" + site.domain);
           var timeOut, intervalID;
-          timeOut = setTimeout(this.timeoutCb(sessionTab(timeOut, intervalID)), scope.currentSite.allocatedTime*1000 * 60);
+          timeOut = setTimeout(function(sessionTab, timeOut, intervalID) {
+            if(!sessionTab.closed) {
+              clearTimeout(timeOut);
+              clearInterval(intervalID);
+              $scope.siteFinished = true;
+              sessionTab.close();
+              var nextSite = PackSessionService.getNextPage();
+              if(nextSite != null) {
+                this.openSite(nextSite);
+              }
+            }
+          }, $scope.currentSite.allocatedTime*1000 * 60);
 
           intervalID = window.setInterval(function() {
             if(sessionTab.closed) {
-              scope.$apply(function() {
-                scope.manuallyClosed = true;
+              $scope.$apply(function() {
+                $scope.manuallyClosed = true;
               });
               clearTimeout(timeOut);
               clearInterval(intervalID);
             }
           }, 500); //check to see if window was closed
-        };
-
-        this.timeoutCb = function(sessionTab, timeOut, intervalID) {
-          if(!sessionTab.closed) {
-            clearTimeout(timeOut);
-            clearInterval(intervalID);
-            scope.siteFinished = true;
-            sessionTab.close();
-            var nextSite = PackSessionService.getNextPage();
-            if(nextSite != null) {
-              this.openSite(nextSite);
-            }
-          }
         };
       },
       link: function (scope, element, attrs, ctrl) {
